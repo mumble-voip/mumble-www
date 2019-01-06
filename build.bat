fcfca22e2a04d8e0187d731373fba0a945eff3d3
Jan Klass <kissaki@posteo.de>
2019-01-06 17:33:53 +0100

    Cross compile and package linux server binary

--- build.bat
+++ build.bat
@@ -11,7 +11,11 @@ POPD
 go build src/mumble-www.go src/githubcache.go src/snapshotcache.go
 IF ERRORLEVEL 1 (PAUSE && EXIT /B 1)
 
+SET GOOS=linux
+go build src/mumble-www.go src/githubcache.go src/snapshotcache.go
+IF ERRORLEVEL 1 (PAUSE && EXIT /B 1)
+
 DEL mumble-www.7z 1>NUL
-7z.exe a mumble-www.7z public mumble-www.exe
+7z.exe a mumble-www.7z public mumble-www
 
 POPD
