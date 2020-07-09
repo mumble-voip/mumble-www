56acaa2ba13dbdc970a91778742d43e08feedb31
Robert Adam <dev@robert-adam.de>
2020-07-09 11:20:16 +0200

    RELEASE: 1.3.2
    This commit updates the respective release information and also creates
    a new blog entry for it.

--- src/downloads.go
+++ src/downloads.go
@@ -7,15 +7,14 @@ import (
 
 func setupDownloadsStable(mux *http.ServeMux) {
 	stable := map[string]string{
-		"windows-32":          "https://github.com/mumble-voip/mumble/releases/download/1.3.1/mumble-1.3.1.msi",
-		"windows-64":          "https://github.com/mumble-voip/mumble/releases/download/1.3.1/mumble-1.3.1.winx64.msi",
-		"osx":                 "https://github.com/mumble-voip/mumble/releases/download/1.3.1/Mumble-1.3.1.dmg",
+		"windows-32":          "https://github.com/mumble-voip/mumble/releases/download/1.3.2/mumble-1.3.2.msi",
+		"windows-64":          "https://github.com/mumble-voip/mumble/releases/download/1.3.2/mumble-1.3.2.winx64.msi",
+		"osx":                 "https://github.com/mumble-voip/mumble/releases/download/1.3.2/Mumble-1.3.2.dmg",
 		"osx-universal":       "https://github.com/mumble-voip/mumble/releases/download/1.2.10/Mumble-Universal-1.2.10.dmg",
 		"ios":                 "http://itunes.apple.com/us/app/mumble/id443472808?ls=1&mt=8",
-		"appimage-x64":        "https://dl.mumble.info/Mumble-1.3.1-x64_64.AppImage",
 		"ubuntu":              "https://launchpad.net/~mumble/+archive/release",
-		"linux-static-server": "https://github.com/mumble-voip/mumble/releases/download/1.3.1/murmur-static_x86-1.3.1.tar.bz2",
-		"osx-static-server":   "https://github.com/mumble-voip/mumble/releases/download/1.3.1/Murmur-OSX-Static-1.3.1.tar.bz2",
+		"linux-static-server": "https://github.com/mumble-voip/mumble/releases/download/1.3.2/murmur-static_x86-1.3.2.tar.bz2",
+		"osx-static-server":   "https://github.com/mumble-voip/mumble/releases/download/1.3.2/Murmur-OSX-Static-1.3.2.tar.bz2",
 	}
 
 	for source, target := range stable {
