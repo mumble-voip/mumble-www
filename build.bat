8c3f37f9ad40e2a4f7a61bb2df4fe12247757beb
Davide Beatrici <git@davidebeatrici.dev>
2021-02-24 01:06:53 +0100

    Remove Go webserver, as there's no need for it anymore
    7d1c85f9e976061aa597644151201503799c0a6d replaced the links served by it.
    
    As for the files served from our GitHub repository (AUTHORS and LICENSE), a Lua script now takes care of doing that.

--- build.bat
+++ build.bat
@@ -8,13 +8,10 @@ hugo
 IF ERRORLEVEL 1 (EXIT /B 1)
 POPD
 
-call build-go.bat
-IF ERRORLEVEL 1 (EXIT /B 1)
-
 DEL mumble-www.7z 1>NUL
 IF ERRORLEVEL 1 EXIT /B 1
 
-7z.exe a mumble-www.7z public mumble-www
+7z.exe a mumble-www.7z public
 IF ERRORLEVEL 1 EXIT /B 1
 
 POPD
--- src/go.mod
+++
@@ -1,8 +0,0 @@
-module github.com/mumble-voip/mumble-www
-
-require (
-	github.com/PuerkitoBio/goquery v1.5.0
-	github.com/google/go-github v17.0.0+incompatible
-	github.com/google/go-querystring v1.0.0 // indirect
-	gopkg.in/yaml.v2 v2.2.2
-)
